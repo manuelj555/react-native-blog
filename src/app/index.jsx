b22fe6c5184dc6def70afac81a02faea9537f06d
maguirre <maguirre@optimeconsulting.com>
2023-08-23 20:33:07 -0500

    Se agrega nativewind

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { Stack } from 'expo-router'
 
 export default function IndexScreen () {
   return (
-    <View>
+    <View className="items-center justify-center flex-1">
+      <Stack.Screen options={{ title: 'My Blog Page' }}/>
       <Text>Bienvenido!</Text>
     </View>
   )
