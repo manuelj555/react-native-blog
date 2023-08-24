3c655255e512d96883efbc7ad3be342deeae1716
maguirre <maguirre@optimeconsulting.com>
2023-08-23 20:25:06 -0500

    Primer commit

---
+++ src/app/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function IndexScreen () {
+  return (
+    <View>
+      <Text>Bienvenido!</Text>
+    </View>
+  )
+}
